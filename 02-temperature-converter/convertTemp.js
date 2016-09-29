0851673cbf73020051a3195e0c346af47ec2285e
Victor Nascimento Bakke <gipphe@gmail.com>
2016-09-29 16:04:00 +0200

    Redid convertTemp completely. Still WIP

--- 02-temperature-converter/convertTemp.js
+++ 02-temperature-converter/convertTemp.js
@@ -1,10 +1,26 @@
 'use strict';
-module.exports = function(fromType, toType, temp) {
-let newTemp;
-	if (fromTemp === 'c') {
-		newTemp = temp * 1.8 + 32;
-	} else if (fromTemp === 'f') {
-		newTemp = (temp - 32) / 1.8;
+const fToc = temp => (temp - 32) / 1.8;
+const cTof = temp => temp * 1.8 + 32;
+const kToc = temp => temp - 273.15;
+const cTok = temp => temp + 273.15;
+const coll = {
+	fToc: fToc,
+	cTof: cTof,
+	kToc: kToc,
+	cTok: cTok
+};
+const isValidType = type => {
+	if (type !== 'c' && type !== 'f' && type !== 'k'){
+		return false;
 	}
+	return true;
+};
+
+module.exports = function(fromType, toType, temp) {
+	if (!isValidType(fromType) || !isValidType(toType)) {
+		throw new Error('fromType of toType is not of a valid type');
+	}
+	let newTemp = coll[fromType + 'To' + toType](temp);
 	newTemp = Math.round(newTemp * 10) / 10;
+	return newTemp;
 };
--- 02-temperature-converter/temperature-converter.js
+++ 02-temperature-converter/temperature-converter.js
@@ -1,7 +1,6 @@
 'use strict';
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
-const StateMachine = require('javascript-state-machine');
 
 console.log('What do you want to convert FROM?');
 console.log('Fahrenheit (F) or Celcius (C)');
@@ -14,29 +13,13 @@ const close = function() {
 	process.exit(0);
 };
 
-const fsm = StateMachine.create({
-	initial: 'from',
-	events: [
-		{ name: 'fromTempInputted', from: 'from',              to: 'to'    },
-		{ name: 'toTempInputted',   from: 'to',                to: 'convert' },
-		{ name: 'reset',            from: ['to', 'convert'],   to: 'from'      }
-	],
-	callbacks: {
-		onfrom: fromTempInput,
-		onto: toTempInput,
-		onreset: reset
-	}
-});
+const FSM = require('./FSM');
+const states = FSM();
 
 rl.setPrompt('>: ');
 rl.prompt();
 
-const isValidInput = function(input) {
-	if (typeof input !== 'string' || input.length > 1 || input.length <= 0) {
-		return false;
-	}
-	return true;
-};
+const isValidInput = require('./isValidInput');
 
 rl.on('line', function(line) {
 	if (!isValidInput(line)) {
@@ -44,4 +27,6 @@ rl.on('line', function(line) {
 		return;
 	}
 	line = line.toLowerCase();
+
+	states(line);
 }).on('close', close);
